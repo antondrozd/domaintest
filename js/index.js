e9f5b013638f9ad19f5cf7cd864d4b7a9976422a
Anton Drozd <anton.drozd.95@gmail.com>
2018-12-10 22:49:00 +0200

    animation

--- js/index.js
+++ js/index.js
@@ -3,55 +3,70 @@ $(document).ready(function() {
         autoplay: true,
         autoplaySpeed: 2000,
         infinite: false,
-        mobileFirst: true,
+        mobileFirst: true
         // waitForAnimate: false
     });
-});
 
-const arr = ['50', '100', '200', '1k', '5k'];
+    const arr = ['50', '100', '200', '1k', '5k'];
 
-$('.slide').each(function(index) {
-    $(this)
-        .find('.count')
-        .text(arr[index] + ' triangles');
-});
-
-arr.forEach(function(count) {
-    $('select').append(`<option>${count}</option>`);
-});
-
-function animate() {
-    let delay = 0;
-    const delayStep = 2500 / $('.animation polygon').length;
-    const appereanceSpeed = 10;
-
-    $('.animation polygon').each(function() {
-        $(this).css({
-            opacity: 0
-        });
-    });
-
-    $('.animation polygon').each(function() {
+    $('.slide').each(function(index) {
         $(this)
-            .delay(delay)
-            .animate(
-                {
-                    opacity: 1
-                },
-                appereanceSpeed
-            );
-
-        delay += delayStep;
+            .find('.count')
+            .text(arr[index] + ' triangles');
     });
-}
 
-$('.animate').on('click', animate);
+    arr.forEach(function(count) {
+        $('select').append(`<option>${count}</option>`);
+    });
 
-$('select').on('change', function() {
-    const count = $(this).val();
+    function animate() {
+        let delay = 0;
+        const duration = $('#duration').val();
+        const delayStep = duration / $('.animation polygon').length;
+        const appereanceSpeed = 10;
 
-    $('.animation').text('Loading...');
-    $('.animation').load(`/img/vector${count}.svg`);
+        $('.animation polygon').each(function() {
+            $(this).css({
+                opacity: 0
+            });
+        });
+
+        $('.animation polygon').each(function() {
+            $(this)
+                .delay(delay)
+                .animate(
+                    {
+                        opacity: 1
+                    },
+                    appereanceSpeed
+                );
+
+            delay += delayStep;
+        });
+    }
+
+    $('.animate').on('click', animate);
+
+    function loadSVG() {
+        const count = $('select').val();
+
+        $('.animation').load(`/img/vector${count}.svg`, function() {
+            setSVGSize();
+        });
+    }
+
+    $('select').on('change', loadSVG);
+
+    function setSVGSize() {
+        const width = $('.animation').width();
+
+        $('svg g').css({
+            transform: `scale(${width / 265}) translate(${width /
+                135}px, ${width / 135}px)`
+        });
+    }
+
+    $(window).on('resize', setSVGSize);
+
+    loadSVG();
 });
-
-$('select').trigger('change');
